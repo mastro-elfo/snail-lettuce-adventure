9500874f5b753907f94f1cbd304d10f0528e21cc
mastro-elfo <francesco.209@gmail.com>
2020-05-20 18:03:50 +0200

    Updates

---
+++ precache-manifest.df9d71c4b8270714990da6a2aa119a3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d6c901361f313b6b855d84caa451b7",
+    "url": "/snail-lettuce-adventure/index.html"
+  },
+  {
+    "revision": "fb8d0df1e700761ec440",
+    "url": "/snail-lettuce-adventure/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aff887e21950e14114e8",
+    "url": "/snail-lettuce-adventure/static/js/2.48995f75.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/snail-lettuce-adventure/static/js/2.48995f75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb8d0df1e700761ec440",
+    "url": "/snail-lettuce-adventure/static/js/main.9e3253e5.chunk.js"
+  },
+  {
+    "revision": "19962df5dd880b63d453",
+    "url": "/snail-lettuce-adventure/static/js/runtime-main.be0df755.js"
+  }
+]);
