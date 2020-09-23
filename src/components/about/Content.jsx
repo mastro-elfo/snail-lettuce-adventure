e2d5cfb73f6720027b8171775f230fb62e0062ec
mastro-elfo <francesco.209@gmail.com>
2020-09-23 10:56:18 +0200

    Register service worker: SLA now works offline

--- src/components/about/Content.jsx
+++ src/components/about/Content.jsx
@@ -14,38 +14,42 @@ export default function AboutContent() {
 }
 
 const md = `
+### New in version 1.7.0
+
+* Register service worker: SLA now works offline.
+
 ### New in version 1.6.1
 
 * Notify when a new version is released and installed;
 * A new beautiful icon.
 
-### New in version 1.6
+### New in version 1.6.0
 
 * Fix: wrong "suspend until" evaluation when "actual date/time" is out of working hours;
 * Fix reload button in error page not reloading;
 * Update regular expression with spaces between digits and letters.
 
-### New in version 1.5
+### New in version 1.5.0
 
 * Fix bug: TypeError: can't set option weekday when dateStyle or timeStyle is used.
 
-### New in version 1.4
+### New in version 1.4.0
 
 * Add validation of working hours;
 * Also display a message if validation fails;
 * Add "Terms" sections to help page.
 
-### New in version 1.3
+### New in version 1.3.0
 
 * Bug fix.
 
-### New in version 1.2
+### New in version 1.2.0
 
 * When a date/time is out of working hour text is red and shows a message;
 * Add working hour table under settings;
 * Correct evaluation of "Suspend until" date/time.
 
-### New in version 1.1
+### New in version 1.1.0
 
 * Actual date and time freeze on load and can be reload with a button;
 * Remaining SLA now supports weeks;
