cc91881c0af72d9fec05f00725b6ebc65e90aee3
mastro-elfo <francesco.209@gmail.com>
2020-09-29 10:50:17 +0200

    Set debug: false

--- src/i18n.js
+++ src/i18n.js
@@ -22,7 +22,7 @@ i18n
   .init({
     fallbackLng: "en",
     joinArrays: "\n",
-    debug: true,
+    debug: false,
     interpolation: {
       escapeValue: false // not needed for react as it escapes by default
     },
